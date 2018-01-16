6d33c2d2f27ba36f96f099f4d80d556aa9113b6d
raybebop <raybebop@gmail.com>
2018-01-16 11:14:48 +0800

    add comment modified by rr

--- fetchXueqiuKLine.py
+++ fetchXueqiuKLine.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python
 #-*-coding:utf8-*-
 #functions of getting day/week/month k-line of stock
+#modified by rr
 
 from ussBase import *
+
+def fetchKLine(symbol, period, begin_date):
+    _status, cookie = setCookie()
+    if _status != 200:
+        print 'ERROR: set cookie failed'
+        sys.exit(1)
+    HTTP_HEADERS['cookie'] = cookie
+
+    begin_dt = time.strptime(begin_date, '%Y-%m-%d')
+    end_dt = time.localtime()
+    begin_ts = int(time.mktime(begin_dt))*1000
+    end_ts = int(time.mktime(end_dt))*1000
+    underline = int(time.time())*1000
+
+    query = {
+        'SYMBOL': symbol,
+        'PERIOD': period,
+        'BEGIN_TS': begin_ts,
+        'END_TS': end_ts,
+        'UNDERLINE': underline,
+    }
+    url = genUrlByTemplate(HTTP_URI['stockdata'], query)
+    #print url
+    res = http_get(url, HTTP_HEADERS)
+    return res
 
 def main():
     valid_period = ['day', 'week', 'month']
@@ -15,29 +41,7 @@ def main():
         print 'Usage: %s [day|week|month]\n' % sys.argv[0]
         sys.exit(1)
 
-    _status, cookie = setCookie()
-    if _status != 200:
-        print 'ERROR: set cookie failed'
-        sys.exit(1)
-    HTTP_HEADERS['cookie'] = cookie
-
-    begin_dt = time.strptime('2017-01-01', '%Y-%m-%d')
-    end_dt = time.localtime()
-    begin_ts = int(time.mktime(begin_dt))*1000
-    end_ts = int(time.mktime(end_dt))*1000
-    underline = int(time.time())*1000
-    symbol = 'WB'
-
-    query = {
-        'SYMBOL': symbol,
-        'PERIOD': period,
-        'BEGIN_TS': begin_ts,
-        'END_TS': end_ts,
-        'UNDERLINE': underline,
-    }
-    url = genUrlByTemplate(HTTP_URI['stockdata'], query)
-    print url
-    res = http_get(url, HTTP_HEADERS)
+    res = fetchKLine('WB', period, '2017-01-01')
     if res.status_code == 200:
         data = json.loads(res.content)
         print json.dumps(data, indent=4)
