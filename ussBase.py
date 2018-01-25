4310492b50b67bd1a59d92fdc781d5e4b8f4f7aa
raybebop <raybebop@gmail.com>
2018-01-25 15:59:44 +0800

    add import requests

--- ussBase.py
+++ ussBase.py
@@ -7,6 +7,7 @@ import json
 import time
 import math
 
+import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
