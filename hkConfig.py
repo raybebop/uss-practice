620b2e46fb9cb7b465b5bbf0860eb010145e7428
任睿 <renrui@staff.sina.com.cn>
2018-01-29 16:08:38 +0800

    test 4 hkex dayquot

--- hkConfig.py
+++ hkConfig.py
@@ -11,3 +11,20 @@ HKEX_DAYQUOT_TPL = "http://www.hkex.com.hk/eng/stat/smstat/dayquot/d%se.htm"
 
 # fields separator in daily quotation
 HKEX_DAYQUOT_SEP = "-"*79
+
+HKEX_DAYQUOT_TABLE = '''
+TABLE OF CONTENT
+<a href="#market_highlights">MARKET HIGHLIGHTS</a>
+<a href="#quotations">QUOTATIONS</a>
+<a href="#sales_all">SALES RECORDS FOR ALL STOCKS</a>
+<a href="#sales_over">SALES RECORDS OVER $500,000</a>
+<a href="#amendments">AMENDMENT RECORDS FOR TRADE</a>
+<a href="#dealings_suspstocks">DEALINGS IN SECURITIES SUSPENDED DUE TO INSUFFICIENT PUBLIC FLOAT</a>
+<a href="#adj_turnover">ADJUSTED TURNOVER</a>
+<a href="#short_selling">SHORT SELLING TURNOVER - DAILY REPORT</a>
+<a href="#adj_short">PREVIOUS DAY'S ADJUSTED SHORT SELLING TURNOVER</a>
+<a href="#options_exercised">DETAILS OF OPTION EXERCISED ON 26/01/2018</a>
+<a href="#efn_highlights">BOND/NOTES HIGHLIGHTS</a>
+<a href="#overseas_highlights">OVERSEAS TURNOVER HIGHLIGHTS</a>
+<a href="#other_info">OTHER INFORMATION</a>
+'''
--- test_hk.py
+++ test_hk.py
@@ -8,17 +8,71 @@ from bs4 import BeautifulSoup
 from ussBase import HTTP_HEADERS, http_get
 from hkConfig import *
 
+#market_highlights = OrderedDict()
+
+def summaryAnalyzer(ds):
+    r = ds.split('\r\n')
+    print r
+    for i in r:
+        j = i.encode('utf8')
+        if 'Traded' in j:
+            print 'Traded', re.findall(r'Traded: (\d+)', j)
+        if 'Advanced' in j:
+            print 'Advanced', re.findall(r'Advanced\s+: (\d+)', j)
+        if 'Declined' in j:
+            print 'Declined', re.findall(r'Declined\s+: (\d+)', j)
+        if 'Unchanged' in j:
+            print 'Unchanged', re.findall(r'Unchanged\s+: (\d+)', j)
+        if 'HK$' in j:
+            print j
+            print 'HK$', re.findall(r'\(HK$\):(.*)', j)
+        if 'Shares' in j:
+            print 'Shares', re.findall(r'\(Shares\):\s+(.*)', j)
+        if 'Deals' in j:
+            print 'Deals', re.findall(r'\(Deals\):\s+(.*)', j)
+
+
+def indexAnalyzer(di):
+    return di
+
+def dollarsAnalyzer(td):
+    return td
+
+def sharesAnalyzer(ti):
+    return ti
+
 def main():
-    url = HKEX_DAYQUOT_TPL % "180126"
-    print ">>>", url
-    res = http_get(url, HTTP_HEADERS)
-    if res.status_code == 200:
-        html = res.content
-        print html
-        #print json.dumps(html, indent=4)
-    else:
-        print 'ERROR: get stock data failed'
-        sys.exit(1)
+    #url = HKEX_DAYQUOT_TPL % "180126"
+    #print ">>>", url
+    #res = http_get(url, HTTP_HEADERS)
+    #if res.status_code == 200:
+    #    html = res.content
+    #    #print html
+    #    #print json.dumps(html, indent=4)
+    #else:
+    #    print 'ERROR: get stock data failed'
+    #    sys.exit(1)
+
+    html = open('d180126e.htm', 'r').read()
+    soup = BeautifulSoup(html, 'html.parser')
+    mh = soup.find('a', attrs={'name':'market_highlights'})
+    content = mh.next_sibling
+    record = content.split(HKEX_DAYQUOT_SEP)
+
+    daily_summary = record[0]
+    daily_index = record[1]
+    top10_by_dollars = record[2]
+    top10_by_shares = record[3]
+
+    summaryAnalyzer(daily_summary)
+    print '='*77
+    #print indexAnalyzer(daily_index)
+    #print '='*77
+    #print dollarsAnalyzer(top10_by_dollars)
+    #print '='*77
+    #print sharesAnalyzer(top10_by_shares)
+    #print '='*77
+
 
 if __name__ == '__main__':
     main()
