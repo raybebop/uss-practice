196b31f5a55bd56fade519a108d713aff3eb8a6b
任睿 <renrui@staff.sina.com.cn>
2018-03-21 10:31:50 +0800

    add dot before xqlib

--- rrstock/__init__.py
+++ rrstock/__init__.py
@@ -17,11 +17,11 @@ from .utillib import (
     genUrlByTemplate,
 )
 
+from .xqlib import *
+
 from .envlib import *
 
 from .hkexlib import *
-
-from xqlib import *
 
 HTTP_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36',
